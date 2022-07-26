0c624a171a7261de279095ad98e838d884c5ab17
EL1A5 <provinciaelias@gmail.com>
2022-07-25 19:39:41 -0500

    config redes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {BsFacebook} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/elias-roy-903434200/" className='_blank'><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/eliasprovincia/" className='_blank'><BsLinkedin/></a>
         <a href="https://github.com/eliasroy" className='_blank'><BsGithub/></a>
         <a href="https://www.facebook.com/EliasRoyPro" className='_blank'><BsFacebook/></a>
 
